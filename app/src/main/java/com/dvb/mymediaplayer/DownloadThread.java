3e990ea09cf62bb60f03f391a5e917c8ada7b15f
BondarenkoDmitry <demianbondarenko@gmail.com>
2018-07-03 12:03:01 +0300

    Initial commit

---
+++ app/src/main/java/com/dvb/mymediaplayer/DownloadThread.java
@@ -0,0 +1,19 @@
+package com.dvb.mymediaplayer;
+
+import android.os.Looper;
+import android.util.Log;
+
+public class DownloadThread extends Thread {
+    private static final String TAG = DownloadThread.class.getSimpleName();
+
+    public DownloadHandler mHandler;
+
+    @Override
+    public void run() {
+        Looper.prepare();
+        mHandler = new DownloadHandler();
+        Looper.loop();
+    }
+
+
+}
---
+++ app/src/main/java/com/dvb/mymediaplayer/Playlist.java
@@ -0,0 +1,11 @@
+package com.dvb.mymediaplayer;
+
+public class Playlist {
+    public static String [] songs = {"He's the Greatest Dancer",
+            "Lover lover lover",
+            "Here comes the Sun",
+            "Songs of love and hate",
+            "If it be your will",
+            "Land of Plenty"
+    };
+}
